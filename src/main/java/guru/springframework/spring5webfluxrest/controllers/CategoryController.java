d20a4511495f372a6a50f700dd40cde2a5d2de3d
Mohamed <mohamed.e@trufla.com>
2019-07-08 14:30:30 +0200

    Added Category create/ update endpoints added unit tests for create/ update controllers

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
@@ -3,9 +3,9 @@ package guru.springframework.spring5webfluxrest.controllers;
 
 import guru.springframework.spring5webfluxrest.domain.Category;
 import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,5 +27,17 @@ public class CategoryController {
     @GetMapping("/api/v1/categories/{id}")
     Mono<Category> getById(@PathVariable String id){
         return categoryRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories")
+    Mono<Void> create(@RequestBody Publisher<Category> categoryStream){
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+
+    @PutMapping("/api/v1/categories/{id}")
+    Mono<Category> update(@PathVariable String id,@RequestBody Category category){
+        category.setId(id);
+        return categoryRepository.save(category);
     }
 }
--- src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class CategoryControllerTest {
 
@@ -48,5 +50,35 @@ public class CategoryControllerTest {
                 .uri("/api/v1/categories/ssss")
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void create() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().description("desc").build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().description("Cat").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void update() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().description("desc").build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().description("Cat").build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories/saassasa")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
