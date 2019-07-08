ee56b57cae147f5f81da4fc6f46528ec9d05f407
Mohamed <mohamed.e@trufla.com>
2019-07-08 12:32:36 +0200

    Added Vendor Controller findAll/ findById Added Unit tests for Vendor Controller

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,49 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @Before
+    public void setUp() throws Exception {
+        vendorRepository = BDDMockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    public void list() {
+
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("Heb1").build(),
+                        Vendor.builder().firstName("Heb2").build()));
+        webTestClient.get()
+                .uri("/api/v1/vendors/")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+
+        BDDMockito.given(vendorRepository.findById(ArgumentMatchers.anyString()))
+                .willReturn(Mono.just(Vendor.builder().firstName("Heb1").build()));
+        webTestClient.get()
+                .uri("/api/v1/vendors/ssss")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+}
